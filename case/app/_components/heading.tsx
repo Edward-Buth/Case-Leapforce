42b54ab7de3b519a5179b7c262e26d15ec4a3667
Edward Buth <edwardbuth@gmail.com>
2024-01-07 03:17:29 +0100

    fished layout for mobile, tablet and desktop

--- case/app/_components/heading.tsx
+++ case/app/_components/heading.tsx
@@ -2,14 +2,14 @@
 
 export const Heading = () => {
     return (
-        <div className="max-w-xl space-y-4 sm:max-w-96 pb-10">
-            <h1 className="text-3xl md:text-4xl sm:text-3xl font-bold">
+        <div className="max-w-80 space-y-7 sm:max-w-96 pb-10">
+            <h1 className="text-4xl lg:text-3xl sm:text-3xl font-bold">
                Mandatory title about the <span className="font-normal">call-to-action</span> here 
             </h1>
-            <h3 className="text-base sm:text-[13px] md:text-2xl font-medium">
+            <h3 className="text-base sm:text-[13px] lg:text-[15px] font-light">
                 Optional body copy goes here.
             </h3>
-            <button className="text-orange-500"> Optional button -&gt; </button>
+            <button className="text-orange-500 text-[13px]"> Optional button -&gt; </button>
         </div>
 
     )
--- case/app/_components/heroes.tsx
+++ case/app/_components/heroes.tsx
@@ -1,45 +1,92 @@
+import { Button } from "@/components/ui/button";
 import { div } from "@tensorflow/tfjs";
 import Image from "next/image";
 
+// Images with their text and button included
 export const Heroes = () => {
     return (
         <div className="flex flex-col items-center justify-center max-w-5xl">
-            <div className="flex flex-wrap items-center">
-                <div className="relative w-[400px] h-[400px]
-                 sm:w-[0px] sm:h-[0px] md:w-[500px] md:h-[380px]">
+            <div className="grid sm:flex lg:flex items-center">
+
+                {/* desktop & mobile */}
+
+                <div className="relative grid w-[400px] h-[320px] mb-5
+                 sm:hidden lg:grid lg:mb-0 lg:mr-3 lg:w-[470px] lg:h-[380px]">
                     <Image
                     src="/Image-2.png"
                     fill
-                    className="object-contain"
+                    className="object-contain z-10"
                     alt="image"
                     />
+                    {/* title, description, button */}
+                    <div className="grid grid-cols-5 gap-3 z-20 h-30 bg-gradient-to-t from-neutral-800 self-end">
+                        <h2 className="col-span-4 text-white text-left text-xl ml-5">Title</h2>
+                        <p className="col-span-4 text-neutral-400 text-left ml-5">
+                            Supporting text about the call-to-action goes here.
+                        </p>
+                        <Button variant={"outline"} className=" w-14 h-14 bg-transparent text-white mb-5">
+                            -&gt;
+                        </Button>
+                    </div>
                 </div>
-                <div className="relative w-[400px] h-[400px]
-                sm:w-[0px] sm:h-[0px] md:w-[500px] md:h-[380px]">
+
+                <div className="relative grid w-[400px] h-[320px]
+                 sm:hidden lg:grid lg:ml-3 lg:w-[470px] lg:h-[380px]">
                     <Image
                     src="/Image-2.png"
                     fill
-                    className="object-contain"
+                    className="object-contain z-10"
                     alt="image"
                     />
+                    <div className="grid grid-cols-5 gap-3 z-20 h-30 bg-gradient-to-t from-neutral-800 self-end">
+                        <h2 className="col-span-4 text-white text-left text-xl ml-5">Title</h2>
+                        <p className="col-span-4 text-neutral-400 text-left ml-5">
+                            Supporting text about the call-to-action goes here.
+                        </p>
+                        <Button variant={"outline"} className=" w-14 h-14 bg-transparent text-white mb-5">
+                            -&gt;
+                        </Button>
+                    </div>
                 </div>
-                <div className="relative w-[0] h-[0]
-                sm:w-[290px] sm:h-[370px] md:w-[0] md:h-[0]">
+
+                {/* Tablet version */}
+
+                <div className="relative hidden sm:grid
+                sm:w-[320px] sm:h-[425px] sm:mr-2 lg:hidden">
                     <Image
                     src="/Image.png"
                     fill
-                    className="object-contain"
+                    className="object-contain z-10"
                     alt="image"
                     />
+                    <div className="grid grid-cols-4 gap-3 z-20 h-30 bg-gradient-to-t from-neutral-800 self-end">
+                        <h2 className="col-span-3 text-white text-2xl text-left ml-5">Title</h2>
+                        <p className="col-span-3 text-neutral-400 text-left ml-5">
+                            Supporting text about the call-to-action goes here.
+                        </p>
+                        <Button variant={"outline"} className=" w-14 h-14 bg-transparent text-white mb-5">
+                            -&gt;
+                        </Button>
+                    </div>
                 </div>
-                <div className="relative w-[0px] h-[0px]
-                sm:w-[290px] sm:h-[370px] md:w-[0px] md:h-[0px]">
+
+                <div className="relative hidden sm:grid
+                sm:w-[320px] sm:h-[425px] sm:ml-2 lg:hidden">
                     <Image
                     src="/Image.png"
                     fill
-                    className="object-contain"
+                    className="object-contain z-10"
                     alt="image"
                     />
+                    <div className="grid grid-cols-4 gap-3 z-20 h-30 bg-gradient-to-t from-neutral-800 self-end">
+                        <h2 className="col-span-3 text-white text-2xl text-left ml-5">Title</h2>
+                        <p className="col-span-3 text-neutral-400 text-left ml-5">
+                            Supporting text about the call-to-action goes here.
+                        </p>
+                        <Button variant={"outline"} className=" w-14 h-14 bg-transparent text-white mb-5">
+                            -&gt;
+                        </Button>
+                    </div>
                 </div>
             </div>
         </div>
