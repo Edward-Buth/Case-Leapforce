500f6702f84cef3ea8c2585d456d25ea0ffb29db
Edward Buth <edwardbuth@gmail.com>
2024-01-07 04:06:13 +0100

    fonts added

--- case/app/_components/heading.tsx
+++ case/app/_components/heading.tsx
@@ -3,13 +3,13 @@
 export const Heading = () => {
     return (
         <div className="max-w-80 space-y-7 sm:max-w-96 pb-10">
-            <h1 className="text-4xl lg:text-3xl sm:text-3xl font-bold">
-               Mandatory title about the <span className="font-normal">call-to-action</span> here 
+            <h1 className="font-gtafont text-4xl lg:text-3xl sm:text-3xl font-extrabold">
+               Mandatory title about the <span className="font-thin">call-to-action</span> here
             </h1>
-            <h3 className="text-base sm:text-[13px] lg:text-[15px] font-light">
+            <h3 className="font-ttfont text-slate-600 text-base sm:text-[13px] lg:text-[15px] font-light">
                 Optional body copy goes here.
             </h3>
-            <button className="text-orange-500 text-[13px]"> Optional button -&gt; </button>
+            <button className="font-ttfont text-orange-500 text-[13px]"> Optional button -&gt; </button>
         </div>
 
     )
--- case/app/page.tsx
+++ case/app/page.tsx
@@ -4,7 +4,7 @@ import { Heroes } from './_components/heroes';
 
 export default function Home() {
   return (
-    <div className='min-h-full flex flex-col'>
+    <div className='min-h-full flex flex-col font-ttfont'>
       <div className='flex flex-col items-center
       justify-center text-center gap-y-8
       flex-1 px-3 pb-5 md:pt-0 sm:pt-0 pt-20'>
